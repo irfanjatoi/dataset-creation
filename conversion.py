0c0f3be05ef106b0866c6e1c907c95f090fae2b8
irfanjatoi <93938947+irfanjatoi@users.noreply.github.com>
2024-05-02 14:24:58 +0500

    Upload all data files on Movies dataset.
    Scraping, cleaning, wrangling of movies data from wiki.

---
+++ conversion.py
@@ -0,0 +1,48 @@
+import re
+
+amounts = r"thousand|million|billion"
+number = r"\d+(,\d{3})*\.*\d*"
+
+word_re = rf"\${number}(-|\sto\s)?({number})?\s({amounts})"
+value_re = rf"\${number}"
+# '''
+# money_conversion("$12.2 million") --> 12200000 - The WORD syntax
+# money_conversion("$790,000") --> 790000 -        The VALUE syntax
+# '''
+
+
+def word_to_value(word):
+    value_dict = {"thousand": 1000, "million": 1000000, "billion": 1000000000}
+    return value_dict[word]
+    
+def parse_word_syntax(string):
+    value_string = re.search(number, string).group()
+    value = float(value_string.replace(',', ''))
+    word = re.search(amounts, string, flags=re.I).group()
+    word_value = word_to_value(word)
+    return value*word_value
+    
+
+
+
+def parse_value_syntax(string):
+    value_string = re.search(number, string).group()
+    value = float(value_string.replace(',', ''))
+    return value
+    
+def money_conversion(money):
+
+    if isinstance(money, list):
+        money = money[0]
+    
+    word_syntax =  re.search(word_re, money, flags=re.I).lower()
+    value_syntax = re.search(value_re, money)
+
+    if word_syntax:
+        return parse_word_syntax(word_syntax.group())
+
+    elif value_syntax:
+        return parse_value_syntax(value_syntax.group())
+        
+    
+print(money_conversion("$790 millions"))
